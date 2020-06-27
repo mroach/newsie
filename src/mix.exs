e695dfc076a0f2bda0847949c63518b017f2a4c5
Mike Roach <github@c.mroach.com>
2020-06-28 01:48:22 +0200

    chore(ci): add dialyxir (#12)

--- src/mix.exs
+++ src/mix.exs
@@ -12,6 +12,9 @@ defmodule Newsie.MixProject do
       homepage_url: "https://github.com/mroach/newsie",
       docs: [
         main: "Newsie"
+      ],
+      dialyzer: [
+        plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
       ]
     ]
   end
@@ -30,7 +33,8 @@ defmodule Newsie.MixProject do
 
       # env-restricted deps
       {:credo, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.22", only: [:dev], runtime: false}
     ]
   end
 end
