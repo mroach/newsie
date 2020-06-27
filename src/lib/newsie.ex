68b402f4dc78bd721e29de5c311030d246d01555
Mike Roach <github@c.mroach.com>
2020-06-27 11:35:58 +0200

    feat: send user-agent header (#4)

--- src/lib/newsie.ex
+++ src/lib/newsie.ex
@@ -2,4 +2,18 @@ defmodule Newsie do
   @moduledoc """
   Documentation for `Newsie`.
   """
+
+  @version Mix.Project.config()[:version]
+
+  @doc """
+  HTTP User-Agent to send to remote APIs
+
+  ### Example
+      iex> Newsie.user_agent()
+      "Newsie/#{@version}"
+  """
+  @spec user_agent() :: String.t()
+  def user_agent do
+    "Newsie/#{@version}"
+  end
 end
